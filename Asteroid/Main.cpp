2db851f773312c4d7815e99132916cc582d17d67
Tarfax <lostmike@gmail.com>
2021-02-24 12:06:59 +0100

    Adding SDL and core scripts

---
+++ Asteroid/Main.cpp
@@ -0,0 +1,11 @@
+#include <SDL.h>
+#include<iostream>
+
+int main(int argc, char** args) {
+	std::cout << "Press whatever and then enter" << std::endl;
+
+	std::string msg;
+	std::cin >> msg;
+
+	return 0;
+}
