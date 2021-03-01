f8aaf1cb135f13b64ce9e34e0b21bfb9a5fac5b3
Tarfax <lostmike@gmail.com>
2021-03-01 11:52:25 +0100

    RandomFloat

--- Asteroid-Team10/src/Math/Mathf.h
+++ Asteroid-Team10/src/Math/Mathf.h
@@ -1,11 +1,11 @@
 #pragma once
-
+#include <random>
 
 #define PI 3.14159265359f
 class Mathf {
 public:
 
-	static float DegToRad(double degree) {
+	inline static float DegToRad(double degree) {
 		return (degree * (PI / 180));
 	}
 
@@ -14,8 +14,14 @@ public:
 	}
 
 	template <typename T>
-	static int Sign(T val) {
+	inline static int Sign(T val) {
 		return (T(0) < val) - (val < T(0));
+	}
+
+	inline static float RandomFloat() {
+		static std::mt19937 randomEngine;
+		static std::uniform_int_distribution<std::mt19937::result_type> distribution;
+		return (float)distribution(randomEngine) / (float) std::numeric_limits<uint32_t>::max();
 	}
 
 };
